dd32dad38f858c7e2a9610e8fbd8f76caa4dfef4
wilcrofter <bill@crofter.net>
2017-06-01 14:55:41 -0400

    refactor

--- src/RingSandbox.jl
+++
@@ -1,6 +0,0 @@
-module RingSandbox
-
-include("generating_set.jl")
-include("z[x].jl")
-
-end
--- src/ZAdjoin.jl
+++ src/ZAdjoin.jl
@@ -1,55 +1,59 @@
+module ZAdjoin
+
 import Base: +, *, one, string, show, Matrix
 
 ## Types
 
 """ Modulus
 
-   Assuming α^n = p[1] + p[2]α + ... + p[n]α^(n-1), create expressions for α^k, k=n, ... ,2n-2
-"""
+  Assuming α^n = p[1] + p[2]α + ... + p[n]α^(n-1), create expressions for α^k, k=n, ... ,2n-2 and powers 0,...,n-1of p's companion matrix.
+  """
 immutable Modulus{I<:Integer}
-  modulus::Array{Array{I,1},1}
-  matrix::Array{I,2}
+  polynomials::Array{Array{I,1},1}
+  matrices::Array{Array{I,2},1}
   
   function (::Type{Modulus}){I}(p::Array{I,1})
     n = length(p)
     n > 1 || error("n==1, a trivial case, is not supported")
-    modulus = Array(Array{I,1},n-1)
-    matrix = zeros(I,(n,n))
-    modulus[1]=p
-    matrix[:,n]=p
+    polynomials = Array(Array{I,1},n-1)
+    polynomials[1]=p
+    A = zeros(I,(n,n))
+    A[:,n]=p
     if n>1
       for i in 2:(n-1)
-        modulus[i] = vcat([0],modulus[i-1][1:(n-1)])+modulus[i-1][1]*p
-        matrix[i,i-1]=1
+        polynomials[i] = vcat([0],polynomials[i-1][1:(n-1)])+polynomials[i-1][1]*p
+        A[i,i-1]=1
       end
-      matrix[n,n-1]=1
+      A[n,n-1]=1
     end
-    new{I}(modulus,matrix)
+    matrices = Array(Array{I,2},n)
+    for i in 1:n
+      matrices[i]=A^(i-1)
+    end
+    new{I}(polynomials,matrices)
   end
   
 end
 
 immutable Element{I<:Integer}
-  p::Array{I,1}
+  coefficients::Array{I,1}
   modulus::Modulus{I}
   
-  function (::Type{Element}){I}(p::Array{I,1}, modulus::Modulus{I})
-    length(p)==length(modulus.modulus[1]) || error("unequal degrees ", length(p), " and ", length(modulus.modulus[1]),".")
-    new{I}(p,modulus)
+  function (::Type{Element}){I}(coef::Array{I,1}, modulus::Modulus{I})
+    length(coef)==length(modulus.polynomials[1]) || error("unequal degrees ", length(coef), " and ", length(modulus.polynomials[1]),".")
+    new{I}(coef,modulus)
   end
 end
 
 ## Conversion
 
 function Matrix{I}(z::Element{I})
-  p = z.p
+  p = z.coefficients
   n = length(p)
-  A = z.modulus.matrix
-  B = eye(I,n)
-  ans = p[1]*B
-  for i=2:n
-    B*=A
-    ans += p[i]*B
+  matrices = z.modulus.matrices
+  ans = p[1]*matrices[1]
+  for i in 2:n
+    ans += p[i]*matrices[i]
   end
   ans
 end
@@ -58,27 +62,27 @@ end
 
 function +{I<:Integer}(a::Element{I},b::Element{I})
   a.modulus == b.modulus || error("terms are not from the same ring")
-  Element(a.p+b.p,a.modulus)
+  Element(a.coefficients+b.coefficients,a.modulus)
 end
 
 function *{I<:Integer}(a::Element{I},b::Element{I})
   a.modulus == b.modulus || error("factors are not from the same ring")
-  n=length(a.p)
+  n=length(a.coefficients)
   ans = zeros(I,2*n-1)
   # convolve (noting that arrays are 1-origin)
   for k in 2:(2*n)
      for i in max(k-n,1):min(k-1,n)
-       ans[k-1]+=a.p[i]*b.p[k-i]
+       ans[k-1]+=a.coefficients[i]*b.coefficients[k-i]
      end
   end
   for i in 1:(n-1)
-    ans[1:n] += ans[n+i]*a.modulus.modulus[i]
+    ans[1:n] += ans[n+i]*a.modulus.polynomials[i]
   end
   Element(ans[1:n], a.modulus)
 end
 
 function one{I<:Integer}(z::Element{I})
-  p=zeros(I,length(z.p))
+  p=zeros(I,length(z.coefficients))
   p[1]=1
   Element(p,z.modulus)
 end
@@ -89,12 +93,13 @@ function helper{I<:Integer}(p::Array{I,1})
   tmp=""
   for i in eachindex(p)
     if p[i] != 0
+      sgn = p[i] < 0 ? "-" : "+"
       if i==1
         tmp=tmp*"$(p[1])"
       elseif i==2
-        tmp=tmp*"+$(p[i])α"
+        tmp=tmp*"$(sgn)$(abs(p[i]))α"
       else
-        tmp=tmp*"+$(p[i])α^$(i-1)"
+        tmp=tmp*"$(sgn)$(abs(p[i]))α^$(i-1)"
       end
     end
   end
@@ -102,11 +107,11 @@ function helper{I<:Integer}(p::Array{I,1})
 end
 
 function string{I<:Integer}(z::Element{I})
-  helper(z.p)
+  helper(z.coefficients)
 end
 
 function string{I<:Integer}(m::Modulus{I})
-  "α^$(length(m.modulus[1])) = $(helper(m.modulus[1]))"
+  "α^$(length(m.polynomials[1])) = $(helper(m.polynomials[1]))"
 end
 
 function show{I<:Integer}(io::IO, z::Element{I})
@@ -119,3 +124,5 @@ function show{I<:Integer}(io::IO, m::Modulus{I})
   print(io, string(m))
 end
 
+
+end
