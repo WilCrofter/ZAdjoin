3da75e6cb4a616bf555e5f5fcde8d25e0d32ceb5
wilcrofter <bill@crofter.net>
2017-05-31 21:46:06 -0400

    convert Element to Matrix

--- src/z[x].jl
+++ src/z[x].jl
@@ -1,4 +1,4 @@
-import Base: +, *, one, string, show
+import Base: +, *, one, string, show, Matrix
 
 ## Types
 
@@ -8,18 +8,25 @@ import Base: +, *, one, string, show
 """
 immutable Modulus{I<:Integer}
   modulus::Array{Array{I,1},1}
+  matrix::Array{I,2}
+  
   function (::Type{Modulus}){I}(p::Array{I,1})
     n = length(p)
     n > 1 || error("n==1, a trivial case, is not supported")
     modulus = Array(Array{I,1},n-1)
+    matrix = zeros(I,(n,n))
     modulus[1]=p
+    matrix[:,n]=p
     if n>1
       for i in 2:(n-1)
         modulus[i] = vcat([0],modulus[i-1][1:(n-1)])+modulus[i-1][1]*p
+        matrix[i,i-1]=1
       end
+      matrix[n,n-1]=1
     end
-    new{I}(modulus)
+    new{I}(modulus,matrix)
   end
+  
 end
 
 immutable Element{I<:Integer}
@@ -30,6 +37,21 @@ immutable Element{I<:Integer}
     length(p)==length(modulus.modulus[1]) || error("unequal degrees ", length(p), " and ", length(modulus.modulus[1]),".")
     new{I}(p,modulus)
   end
+end
+
+## Conversion
+
+function Matrix{I}(z::Element{I})
+  p = z.p
+  n = length(p)
+  A = z.modulus.matrix
+  B = eye(I,n)
+  ans = p[1]*B
+  for i=2:n
+    B*=A
+    ans += p[i]*B
+  end
+  ans
 end
 
 ## Arithmetic
@@ -63,21 +85,37 @@ end
 
 ## Display
 
-function string{I<:Integer}(z::Element{I})
+function helper{I<:Integer}(p::Array{I,1})
   tmp=""
-  for i in eachindex(z.p)
-    if i==1
-      tmp=tmp*"$(z.p[1])"
-    elseif i==2
-      tmp=tmp*"+$(z.p[i])α"
-    else
-      tmp=tmp*"+$(z.p[i])α^$(i-1)"
+  for i in eachindex(p)
+    if p[i] != 0
+      if i==1
+        tmp=tmp*"$(p[1])"
+      elseif i==2
+        tmp=tmp*"+$(p[i])α"
+      else
+        tmp=tmp*"+$(p[i])α^$(i-1)"
+      end
     end
   end
-  tmp
+  return length(tmp) == 0 ? "0" : tmp
+end
+
+function string{I<:Integer}(z::Element{I})
+  helper(z.p)
+end
+
+function string{I<:Integer}(m::Modulus{I})
+  "α^$(length(m.modulus[1])) = $(helper(m.modulus[1]))"
 end
 
 function show{I<:Integer}(io::IO, z::Element{I})
+  print(io, "Element{$I}\n")
   print(io, string(z))
+end
+
+function show{I<:Integer}(io::IO, m::Modulus{I})
+  print(io, "Modulus{$I}\n")
+  print(io, string(m))
 end
 
